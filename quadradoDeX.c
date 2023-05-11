ab199dfd0ba8517225e958c7cb9b6b270468c3e8
maisagarcx <115797807+maisagarcx@users.noreply.github.com>
2023-05-11 08:58:31 -0300

    quadradoDeX.c

---
+++ quadradoDeX.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(void)
+{
+	int i,j, varia;
+	scanf("%d", &varia);
+	for(i=0;i<varia;i++)
+	{
+		for(j=0;j<varia;j++)
+		{
+			printf("x");
+		}
+		printf("\n");
+	}
+	return 0;
+}
